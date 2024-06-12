9d932a20c8139e591d24ccd6e2ba596115c00771
Kane-dylan <cool.kdtech1234@gmail.com>
2024-06-12 20:40:18 +0530

    updated

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-// function handleMenu() {
-//     console.log('first')
-// }
+// navtoggle
 const hiddenNav = document.getElementById('hiddenNav');
-
 function handleMenu() {
     hiddenNav.classList.toggle('hidden'); 
+}
+
+// option-toggles
+const hiddenOptions = document.getElementById('hiddenOptions');
+function actionMenu() {
+    hiddenOptions.classList.toggle('hidden'); 
+}
+
+// aditional option-toggles
+const dropdownImages = document.getElementById('dropdownImages');
+function toggleDropdown0() {
+    dropdownImages.classList.toggle('hidden');
+}
+const dropdownVideos = document.getElementById('dropdownVideos');
+function toggleDropdown1() {
+    dropdownVideos.classList.toggle('hidden');
+}
+const dropdownGraphics = document.getElementById('dropdownGraphics');
+function toggleDropdown2() {
+    dropdownGraphics.classList.toggle('hidden');
 }
